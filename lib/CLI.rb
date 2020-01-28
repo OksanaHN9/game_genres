bbfe54da4bb79c3f1ded1945e7be21ce558fda2d
Oksana Nichols <oksanahnichols@yahoo.com>
2020-01-28 18:27:28 +0000

    if else statements

--- lib/CLI.rb
+++ lib/CLI.rb
@@ -4,7 +4,8 @@ class GameGenres::CLI
     puts "Hey There Gamers!"
     puts "Here you can look up a list of games based on your genre preference"
     puts "To select a list of genres you can select list_genre"
-    
+    puts "To select a list of titles select title"
+    puts "to select a description of a specific genre select description"
     list_genre 
 end 
 
@@ -12,4 +13,11 @@ end
     GameGenres::Scraper.scrape 
     input = nil 
       while input != "exit"
+    puts ""
+    puts "Enter list_genre"
     
+    if input.list_genre 
+      return genre.list 
+      puts "Here is a list of all our available genres"
+      
+    elsif input ==   
