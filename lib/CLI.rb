16763df58da1df62d4e9285357c59db9067435f9
Oksana Nichols <oksanahnichols@yahoo.com>
2020-01-28 18:19:15 +0000

    define listgenres

--- lib/CLI.rb
+++ lib/CLI.rb
@@ -3,7 +3,13 @@ class GameGenres::CLI
   def call 
     puts "Hey There Gamers!"
     puts "Here you can look up a list of games based on your genre preference"
+    puts "To select a list of genres you can select list_genre"
     
+    list_genre 
 end 
 
-  def 
+  def list_genre
+    GameGenres::Scraper.scrape 
+    input = nil 
+      while input != "exit"
+    
