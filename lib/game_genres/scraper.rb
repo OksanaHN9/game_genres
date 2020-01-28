79e9839679b1d4dcd089d3cb869f31c61d52afee
Oksana Nichols <oksanahnichols@yahoo.com>
2020-01-28 20:49:43 +0000

    scrape of steam store

--- lib/game_genres/scraper.rb
+++ lib/game_genres/scraper.rb
@@ -5,8 +5,25 @@ require 'colorize'
 
 Class GameGenres::Scraper 
 
-
-
+def genre 
+  puts "list of all genres"
+  <div class = "gutter_header pad">Browse_by_genre</div> == $0
   
+end 
+Class Genre
+ def genre_list 
+puts <a class="gutter_item" href="https://store.steampowered.com/genre/Free%20to%20Play/?snr=1_4_4__125">
+puts 	<a class="gutter_item" href="https://store.steampowered.com/genre/Early%20Access/?snr=1_4_4__125">
+puts <a class="gutter_item" href="https://store.steampowered.com/tags/en/Action/?snr=1_4_4__125">
+puts <a class="gutter_item" href="https://store.steampowered.com/tags/en/Adventure/?snr=1_4_4__125">
+puts <a class="gutter_item" href="https://store.steampowered.com/tags/en/Casual/?snr=1_4_4__125">
+puts <a class="gutter_item" href="https://store.steampowered.com/tags/en/Indie/?snr=1_4_4__125">
+puts <a class="gutter_item" href="https://store.steampowered.com/tags/en/Massively%20Multiplayer/?snr=1_4_4__125">
+puts <a class="gutter_item" href="https://store.steampowered.com/tags/en/Racing/?snr=1_4_4__125">
+puts <a class="gutter_item" href="https://store.steampowered.com/tags/en/RPG/?snr=1_4_4__125">
+puts <a class="gutter_item" href="https://store.steampowered.com/tags/en/Simulation/?snr=1_4_4__125">
+puts <a class="gutter_item" href="https://store.steampowered.com/tags/en/Sports/?snr=1_4_4__125">
+puts <a class="gutter_item" href="https://store.steampowered.com/tags/en/Strategy/?snr=1_4_4__125">
+
 
     
