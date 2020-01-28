2dbcff3a0336b94e3bb89a19480838f7e3165986
Oksana Nichols <oksanahnichols@yahoo.com>
2020-01-28 18:31:42 +0000

    slight change to puts

--- lib/CLI.rb
+++ lib/CLI.rb
@@ -3,7 +3,7 @@ class GameGenres::CLI
   def call 
     puts "Hey There Gamers!"
     puts "Here you can look up a list of games based on your genre preference"
-    puts "To select a list of genres you can select list_genre"
+    puts "To select a list of genres you can select genre"
     puts "To select a list of titles select title"
     puts "to select a description of a specific genre select description"
     list_genre 
@@ -14,10 +14,13 @@ end
     input = nil 
       while input != "exit"
     puts ""
-    puts "Enter list_genre"
+    puts "Enter genre"
     
     if input.list_genre 
       return genre.list 
       puts "Here is a list of all our available genres"
       
-    elsif input ==   
+    elsif input == "genre"  
+      list_genres 
+    elsif input == "exit"  
+      returning to main menu 
