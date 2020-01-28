1d99b144c42a705a4ece8ee5fbdd2049791c2576
Oksana Nichols <oksanahnichols@yahoo.com>
2020-01-28 16:29:42 +0000

    adding how many titles are available for that genre

--- lib/game_genres.rb
+++ lib/game_genres.rb
@@ -10,8 +10,13 @@ class GameGenre::CLI
     puts "Lists 4 genres to choose from"
     @genres = GameGenre::Genre.menu
     @genres.each.with_index(4) do |genres, i|
-      puts "#{i}". 
+      puts "#{i}" . #{genre.action} #{genre.horror} #{genre.fps} #{genre.mmo}
   end
   
 end 
 
+def title 
+  puts "Here are how many tites are available for the genre you selected"
+  @title = Game_Title::Title 
+  @title.each.with_index(4) do |title, i|
+    puts 
