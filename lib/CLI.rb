90cc55ea14c24415465cd30160d517777089cd6a
Oksana Nichols <oksanahnichols@yahoo.com>
2020-01-30 07:04:56 +0000

    fixed spacing of end

--- lib/CLI.rb
+++ lib/CLI.rb
@@ -1,79 +1,94 @@
-class GameGenres::CLI 
-  
-  def call 
-    puts "Hey There Gamers!"
-    puts "Here you can look up a list of games based on your genre preference"
-    puts "To select a list of genres you can select genre"
-    puts "To select a list of titles select title"
-    puts "to select a description of a specific genre select description"
-    genre 
-end 
+module GameRec
 
-  def genre
-    GameGenres::Scraper.scrape 
-    input = nil 
-      while input != "exit"
-    puts ""
-    puts "Enter genre"
+  class CLI 
     
-    if input.genre 
-      return genre.list 
-      puts "Here is a list of all our available genres"
-      
-    elsif input == "genre"  
-      list_genres 
-    elsif input == "exit"  
-      returning to main menu 
-    else 
-      puts "please select either genre or exit"
-    end
-  end   
-end 
-end 
-
-  def title 
-    GameGenres::Scraper.scrape 
-    input = nil 
-      while input != "exit"
-    puts ""
-    puts "Enter title"
-    
-    if input.title 
-      return title.list 
-      puts "Here is a list of available titles"
-      
-    elsif input == "title" 
-      list_titles 
-    elsif input == "exit"
-      returning to main menu 
-    else 
-      puts "please select either title or exit"
+    def start 
+      puts "Hey There Gamers!"
+      puts "Here you can look up a list of games based on your genre preference"
+      puts "To select a list of genres you can select genre"
+      puts "To select a list of titles select title"
+      puts "to select a description of a specific genre select description"
+      genre 
     end 
-  end   
-end       
-end 
 
-  def description 
-    GameGenres::Scraper.scrape 
-    input = nil 
-      while input != "exit"
-      puts ""
-      puts "Enter Genre"
-      puts "Enter title"
-      puts "Select description"
-   if input.description 
-      puts "select either genre or title or exit" 
+    def genre
+      Scraper.scrape 
+      input = nil 
+        while input != "exit"
+          puts ""
+          puts "Enter genre"
       
-   elsif input == "genre"
-      puts "select genre you want a description of"
-   elsif input == "title"  
-      puts "select title you want a description of"
-   else 
-     puts "please select either genre, title, or exit"    
+          if input.genre 
+            return genre.list 
+          puts "Here is a list of all our available genres"
+        
+          elsif input == "genre"  
+            list_genres 
+          elsif input == "exit"  
+            returning to main menu 
+          else 
+            puts "please select either genre or exit"
+          end
+        end   
     end 
+
+    def title 
+      Scraper.scrape 
+      input = nil 
+        while input != "exit"
+          puts ""
+          puts "Enter title"
+      
+          if input.title 
+            return title.list 
+            puts "Here is a list of available titles"
+        
+          elsif input == "title" 
+            list_titles 
+          elsif input == "exit"
+            returning to main menu 
+          else 
+            puts "please select either title or exit"
+          end 
+        end   
+    end        
+
+    def description 
+      Scraper.scrape 
+      input = nil 
+      while input != "exit"
+        puts ""
+        puts "Enter Genre"
+        puts "Enter title"
+        puts "Select description"
+
+        if input.description 
+          puts "select either genre or title or exit" 
+        elsif input == "genre"
+          puts "select genre you want a description of"
+        elsif input == "title"  
+          puts "select title you want a description of"
+        else 
+          puts "please select either genre, title, or exit"    
+        end 
+      end 
+    end   
   end 
-end   
-end 
+end
+    
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
     
       
       
